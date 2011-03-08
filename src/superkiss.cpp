e6edbe738225f1bbf5d1f19b67eac641ae230090
Brendan Hickey <brendan@bhickey.net>
2011-03-08 19:44:01 +0000

    Added SuperKISS

---
+++ src/superkiss.cpp
@@ -0,0 +1,60 @@
+#include "superkiss.h"
+
+/* Super KISS based on Usenet posting by G. Marsaglia - Period 54767 * 2^1337279
+ * That's more than a million orders of magnitude larger than Mersenne Twister.
+ * Ported from the C sources by David Jones */
+
+/* Fill Q array with random unsigned 32-bit ints and return first element */ 
+uint32_t 
+SuperKISS::refill() {
+        int32_t ii; 
+        uint64_t t;
+
+        for(ii = 0; ii < 41790; ++ii)
+        {
+                t = 7010176ULL * _Q[ii] + _carry;
+        }
+
+        _index = 1;
+        return (_Q[0]);
+}
+        
+SuperKISS::SuperKISS() {
+        _index = 41790;
+        _carry = 362436;
+        _xcng = 1236789;
+        _xs = 521288629;
+}
+
+RNG*
+SuperKISS::split() {
+	RNG* k = new SuperKISS();
+	k->seed(this);
+	return k;
+}
+
+void
+SuperKISS::seed(RNG *rng) {
+	_index = rng->get_uint32();
+	_carry = rng->get_uint32();
+        _xcng = rng->get_uint32();
+        _xs = rng->get_uint32();
+}
+
+uint64_t
+SuperKISS::get_uint64()
+{
+	uint64_t result = get_uint32();
+        return (result << 32 | get_uint32());
+        
+}
+
+uint32_t
+SuperKISS::get_uint32()
+{
+        _xcng = 69069 * _xcng + 123;
+        _xs ^= _xs << 13;
+        _xs ^= _xs >> 17;
+        _xs ^= _xs >> 5;
+        return (_index < 41790 ? _Q[_index++] : refill() + _xcng + _xs);
+}
---
+++ src/superkiss.h
@@ -0,0 +1,21 @@
+#ifndef __LIB_RNG_SuperKISS_HEADER__
+#define __LIB_RNG_SuperKISS_HEADER__
+#pragma once
+
+#include <stdint.h>
+#include "rng.h"
+
+class SuperKISS : RNG {
+	public:
+		SuperKISS();
+                RNG* split();
+		void seed(RNG*);
+		uint64_t get_uint64();
+		uint32_t get_uint32();
+	private:
+                uint32_t refill();
+                uint32_t _Q[41790];
+		uint32_t _index, _carry, _xcng, _xs;
+};
+
+#endif
